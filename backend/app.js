73d780f7ce8826f3e7c499007e507769a6853d42
Grégory MINET <minet.gregory@gmail.com>
2021-11-04 11:14:21 +0100

    route put

--- backend/app.js
+++ backend/app.js
@@ -36,7 +36,13 @@ app.get('/api/sauces/:id', (req, res, next) => {
     Sauce.findOne({ _id: req.params.id })
       .then(sauce => res.status(200).json(sauce))
       .catch(error => res.status(404).json({ error }));
-  });
+});
+
+app.put('/api/sauces/:id', (req, res, next) => {
+    Sauce.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
+      .then(() => res.status(200).json({ message: 'Sauce modifiée !'}))
+      .catch(error => res.status(400).json({ error }));
+});
 
 app.get('/api/sauces', (req, res, next) => {
     Sauce.find()
