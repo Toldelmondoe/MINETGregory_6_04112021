8b00579a3021f1c172375797286b7be2eee39828
Grégory MINET <minet.gregory@gmail.com>
2021-11-04 13:47:24 +0100

    config multer en post

--- backend/routes/sauce.js
+++ backend/routes/sauce.js
@@ -3,8 +3,9 @@ const router = express.Router();
 
 const sauceCtrl = require('../controllers/sauce');
 const auth = require('../middleware/auth');
+const multer = require('../middleware/multer-config');
 
-router.post('/', auth, sauceCtrl.createSauce);
+router.post('/', auth, multer, sauceCtrl.createSauce);
 router.get('/:id', auth, sauceCtrl.getOneSauce);
 router.put('/:id', auth, sauceCtrl.modifySauce);
 router.delete('/:id', auth, sauceCtrl.deleteSauce);
