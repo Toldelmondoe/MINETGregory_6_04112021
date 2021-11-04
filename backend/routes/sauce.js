f7189006db29503bf62c542adb86614db3afc83a
Grégory MINET <minet.gregory@gmail.com>
2021-11-04 13:30:06 +0100

    middleware d'authentification sauf GET

--- backend/routes/sauce.js
+++ backend/routes/sauce.js
@@ -2,11 +2,12 @@ const express = require('express');
 const router = express.Router();
 
 const sauceCtrl = require('../controllers/sauce');
+const auth = require('../middleware/auth');
 
-router.post('/', sauceCtrl.createSauce);
-router.get('/:id', sauceCtrl.getOneSauce);
-router.put('/:id', sauceCtrl.modifySauce);
-router.delete('/:id', sauceCtrl.deleteSauce);
+router.post('/', auth, sauceCtrl.createSauce);
+router.get('/:id', auth, sauceCtrl.getOneSauce);
+router.put('/:id', auth, sauceCtrl.modifySauce);
+router.delete('/:id', auth, sauceCtrl.deleteSauce);
 router.get('/', sauceCtrl.getAllSauces);
 
 module.exports = router;
